c08c25fe672d33689f2f7528c63bb5da9f4d1c5e
李燕 <liyan@greenline.work>
2026-08-14 10:50:50 +0800

    Add 5 GREENLINE machinery catalogues (flipbook hub + books)

---
+++ catalog/catalogs.js
@@ -0,0 +1,65 @@
+window.CATALOG_CATS = {
+  "industrial": {
+    "cn": "工业机械",
+    "en": "Industrial Machinery"
+  },
+  "auto": {
+    "cn": "汽配",
+    "en": "Auto Parts"
+  },
+  "brick": {
+    "cn": "制砖机械",
+    "en": "Brick Machinery"
+  },
+  "package": {
+    "cn": "包装机械",
+    "en": "Packaging Machinery"
+  }
+};
+window.CATALOGS = [
+  {
+    "folder": "engineering-machinery",
+    "category": "industrial",
+    "title_cn": "工程机械产品目录",
+    "title_en": "ENGINEERING MACHINERY CATALOGUE",
+    "pages": 20,
+    "cover_cn": "./engineering-machinery/cover_cn.png",
+    "cover_en": "./engineering-machinery/cover_en.png"
+  },
+  {
+    "folder": "shandong-bochuang",
+    "category": "industrial",
+    "title_cn": "山东博创产品画册",
+    "title_en": "SHANDONG BOCHUANG CATALOGUE",
+    "pages": 31,
+    "cover_cn": "./shandong-bochuang/cover_cn.png",
+    "cover_en": "./shandong-bochuang/cover_en.png"
+  },
+  {
+    "folder": "auto-parts",
+    "category": "auto",
+    "title_cn": "汽配目录册 2020",
+    "title_en": "AUTO PARTS CATALOGUE 2020",
+    "pages": 128,
+    "cover_cn": "./auto-parts/cover_cn.png",
+    "cover_en": "./auto-parts/cover_en.png"
+  },
+  {
+    "folder": "bricks-machine",
+    "category": "brick",
+    "title_cn": "制砖机目录册",
+    "title_en": "BRICKS MACHINE CATALOGUE",
+    "pages": 64,
+    "cover_cn": "./bricks-machine/cover_cn.png",
+    "cover_en": "./bricks-machine/cover_en.png"
+  },
+  {
+    "folder": "package-machine",
+    "category": "package",
+    "title_cn": "包装机械目录册 2026",
+    "title_en": "PACKAGE MACHINE CATALOGUE 2026",
+    "pages": 66,
+    "cover_cn": "./package-machine/cover_cn.png",
+    "cover_en": "./package-machine/cover_en.png"
+  }
+];
